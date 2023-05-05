9be3fd31d79200c1e3f84a0d8ceb286e92b113cc
Philippe <philippe.idalgoprestes@gmail.com>
2023-05-05 16:59:50 -0300

    .

--- src/posts.js
+++ src/posts.js
@@ -1,18 +1,26 @@
 export default function Posts(){
     const users_post_like = [['meowed', 'gato-telefone.svg', ['101.523', 'respondeai']], ['barked', 'dog.svg', ['99.159', 'adorable_animals']]];
     function like_reaction(e){
-      let btn = e.target;
-      btn.setAttribute("style", "background-color: rgb(255, 50, 50);");
-      btn.setAttribute("onClick", "{(e) => deslike_reaction(e)}");
-      let numbers_likes = Number(String(document.getElementById("likes").innerHTML).replaceAll(".", '')) + 1;
-      document.getElementById("likes").innerHTML = numbers_likes;
+      let btn = e.target.id;
+      let numbers_likes;
+      if (document.getElementById(btn).style.backgroundColor == "rgb(255, 50, 50)"){
+        document.getElementById(btn).setAttribute("style", "background-color: rgb(255, 255, 255);");
+        numbers_likes = Number(String(document.getElementById('likes' + btn.split('likebtn')[1]).innerHTML).replaceAll(".", '')) - 1;
+      }
+      else{
+        document.getElementById(btn).setAttribute("style", "background-color: rgb(255, 50, 50);");
+        numbers_likes = Number(String(document.getElementById('likes' + btn.split('likebtn')[1]).innerHTML).replaceAll(".", '')) + 1;
+      }
+      document.getElementById('likes' + btn.split('likebtn')[1]).innerHTML = numbers_likes;
     }
-    function deslike_reaction(e){
-      let btn = e.target;
-      btn.setAttribute("style", "background-color: rgb(255, 255, 255);");
-      btn.setAttribute("onClick", "{(e) => like_reaction(e)}");
-      let numbers_likes = Number(String(document.getElementById("likes").innerHTML).replaceAll(".", '')) - 1;
-      document.getElementById("likes").innerHTML = numbers_likes;
+    function save_reaction(e){
+      let btn = e.target.id;
+      if (document.getElementById(btn).style.backgroundColor == "rgb(0, 0, 0)"){
+        document.getElementById(btn).setAttribute("style", "background-color: rgb(255, 255, 255);");
+      }
+      else{
+        document.getElementById(btn).setAttribute("style", "background-color: rgb(0, 0, 0);");
+      }
     }
     return (
         <div>
@@ -35,19 +43,19 @@ export default function Posts(){
             <div class="fundo">
               <div class="acoes">
                 <div>
-                  <ion-icon name="heart-outline" data-test="like-post" onClick={(e) => like_reaction(e)}></ion-icon>
+                  <ion-icon name="heart-outline" data-test="like-post" id={"likebtn" + data[0] + data[1]} onClick={(e) => like_reaction(e)}></ion-icon>
                   <ion-icon name="chatbubble-outline"></ion-icon>
                   <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
                 <div>
-                  <ion-icon name="bookmark-outline" data-test="save-post"></ion-icon>
+                  <ion-icon name="bookmark-outline" data-test="save-post" id={"savebtn" + data[0] + data[1]} onClick={(e) => save_reaction(e)}></ion-icon>
                 </div>
               </div>
 
               <div class="curtidas">
                 <img src={"assets/img/" + data[2][1] + ".svg"} alt={data[2][1]}/>
                 <div class="texto">
-                  Curtido por <strong>{data[2][1]}</strong> e <strong data-test="likes-number">outras <span id="likes">{data[2][0]}</span> pessoas</strong>
+                  Curtido por <strong>{data[2][1]}</strong> e <strong data-test="likes-number">outras <span id={"likes" + data[0] + data[1]}>{data[2][0]}</span> pessoas</strong>
                 </div>
               </div>
             </div>
